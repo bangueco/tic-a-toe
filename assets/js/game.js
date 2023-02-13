6bc027e3cc8290ac30fb14c32049fe9b188e7528
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-13 13:43:17 +0800

    Add ability to write X and O in boxes

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,6 +6,19 @@
 let player_x = 1;
 let player_o = 0;
 
+// Queries
+let a1 = document.querySelector("#a-1");
+let a2 = document.querySelector("#a-2");
+let a3 = document.querySelector("#a-3");
+
+let b1 = document.querySelector("#b-1");
+let b2 = document.querySelector("#b-2");
+let b3 = document.querySelector("#b-3");
+
+let c1 = document.querySelector("#c-1");
+let c2 = document.querySelector("#c-2");
+let c3 = document.querySelector("#c-3");
+
 function playerMoveX(move) {
   player_x = move;
 }
@@ -14,20 +27,22 @@ function playerMoveO(move) {
   player_o = move;
 }
 
-function playerTurns() {
+function playerTurns(move) {
   if (player_x == 1 && player_o == 0) {
     // Player X always goes first
-    console.log("X");
+    move.textContent = "X";
+    move.classList.add("disable-buttons");
     playerMoveX(0); // Disables player x move
     playerMoveO(1); // Enables player o move
   } else {
-    console.log("Y");
     // Player O always second
+    move.textContent = "O";
+    move.classList.add("disable-buttons");
     playerMoveX(1); // Enables player x move
     playerMoveO(0); // Disables player o move
   }
 }
 
-function clickBox() {
-  playerTurns();
+function clickBox(move) {
+  playerTurns(move);
 }
