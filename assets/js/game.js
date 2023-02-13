4728bf1ff383dd8315ae9d2831d7e41b15dcfd0c
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-02-13 14:24:00 +0800

    Add function to disable boxes

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,6 +19,7 @@ let c1 = document.querySelector("#c-1");
 let c2 = document.querySelector("#c-2");
 let c3 = document.querySelector("#c-3");
 
+let boxes = document.querySelectorAll(".box");
 function playerMoveX(move) {
   player_x = move;
 }
@@ -48,35 +49,57 @@ function clickBox(move) {
 
   if (a1.textContent == "X" && a2.textContent == "X" && a3.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (b1.textContent == "X" && b2.textContent == "X" && b3.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (c1.textContent == "X" && c2.textContent == "X" && c3.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (a1.textContent == "X" && b1.textContent == "X" && c1.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (a2.textContent == "X" && b2.textContent == "X" && c2.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (a3.textContent == "X" && b3.textContent == "X" && c3.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (a1.textContent == "X" && b2.textContent == "X" && c3.textContent == "X") {
     alert("WORKS");
+    disableBoxes();
   } else if (a3.textContent == "X" && b2.textContent == "X" && c1.textContent == "X") {
-    alert("WORKS"); // X
+    alert("WORKS");
+    disableBoxes();
   } else if (a1.textContent == "O" && a2.textContent == "O" && a3.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (b1.textContent == "O" && b2.textContent == "O" && b3.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (c1.textContent == "O" && c2.textContent == "O" && c3.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (a1.textContent == "O" && b1.textContent == "O" && c1.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (a2.textContent == "O" && b2.textContent == "O" && c2.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (a3.textContent == "O" && b3.textContent == "O" && c3.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (a1.textContent == "O" && b2.textContent == "O" && c3.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
   } else if (a3.textContent == "O" && b2.textContent == "O" && c1.textContent == "O") {
     alert("WORKS");
+    disableBoxes();
+  }
+}
+
+function disableBoxes() {
+  for (let listOfNodes = 0; listOfNodes <= 8; listOfNodes++) {
+    boxes[listOfNodes].classList.add("disabled");
   }
 }
